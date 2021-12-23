2f45742181d758f96fe10ea2a4b6ca8d47da57b4
enutbrown <erica.nutbrown0424@gmail.com>
2021-12-22 22:29:06 -0800

    wrote some questionable code

--- script.js
+++ script.js
@@ -1,6 +1,27 @@
 let computerPlay = function() {
-    let options = [['Rock'], ['Paper'], ['Scissors']]
+    let options = [['rock'], ['paper'], ['scissors']]
     let random = options[Math.floor(Math.random()*options.length)];
     return random;
 }
 
+let playRound = function(playerSelection, computerSelection) {
+    if ('rock', 'scissors') {
+        return 'You Win! Rock beats Scissors!'
+    } else if ('rock', 'paper') {
+        return 'You Lose! Paper beats Rock!';
+    } else if ('paper', 'rock') {
+        return 'You Win! Paper beats Rock!';
+    } else if ('paper', 'scissors') {
+        return 'You Lose! Scissors beats Paper!';
+    } else if ('scissors', 'paper') {
+        return 'You Win! Scissors beats Paper!';
+    } else if ('scissors', 'rock') {
+        return 'You Lose! Rock beats Scissors!';
+    } else {
+        return 'Let\'s Play!'
+    }
+}
+
+const playerSelection = 'rock';
+const computerSelection = computerPlay();
+// console.log(playRound(playerSelection, computerSelection));
