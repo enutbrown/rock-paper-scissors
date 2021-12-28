39a653489e31d190dd5da53028a72009137e0436
enutbrown <erica.nutbrown0424@gmail.com>
2021-12-28 00:05:18 -0800

    configured function to play 5 times and track score

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ let computerPlay = function() {
 // variables
 const typedPlayerSelection = 'rock';
 const playerSelection = typedPlayerSelection.toLowerCase();
-const computerSelection = computerPlay();
+let computerSelection = computerPlay();
 const playerWin = 'You Win!';
 const compWin = 'You Lose!'
 let playerScore = 0;
@@ -19,35 +19,31 @@ let compScore = 0;
 //plays one round of rock paper scissors, outputs who wins
 let playRound = function(playerSelection, computerSelection) {
     if (playerSelection == 'rock' && computerSelection == 'scissors') {
-        return `${playerWin} Rock beats Scissors!` + ` Player Score: ${playerScore} `;
+        return `${playerWin} Rock beats Scissors!` + ++playerScore + ` Player Score: ${playerScore} `;
     } else if (playerSelection == 'rock' && computerSelection == 'paper') {
-        return `${compWin} Paper beats Rock!` + ` Computer Score: ${compScore} `;
+        return `${compWin} Paper beats Rock!` + ++compScore + ` Computer Score: ${compScore} `;
     } else if (playerSelection == 'paper' && computerSelection == 'rock') {
-        return `${playerWin} Paper beats Rock!` + ` Player Score: ${playerScore} `;
+        return `${playerWin} Paper beats Rock!` + ++playerScore + ` Player Score: ${playerScore} `;
     } else if (playerSelection == 'paper' && computerSelection == 'scissors') {
-        return `${compWin} Scissors beats Paper!` + ` Computer Score: ${compScore} `;
+        return `${compWin} Scissors beats Paper!` + ++compScore + ` Computer Score: ${compScore} `;
     } else if (playerSelection == 'scissors' && computerSelection == 'paper') {
-        return `${playerWin} Scissors beats Paper!` + ` Player Score: ${playerScore} `;
+        return `${playerWin} Scissors beats Paper!` + ++playerScore + ` Player Score: ${playerScore} `;
     } else if (playerSelection == 'scissors' && computerSelection == 'rock') {
-        return `${compWin} Rock beats Scissors!` + ` Computer Score: ${compScore} `;
+        return `${compWin} Rock beats Scissors!` + ++compScore + ` Computer Score: ${compScore} `;
     } else if (playerSelection == ' ' && computerSelection == 'rock' || 'paper' || 'scissors') {
         return 'Let\'s Play';
     } else if (playerSelection ===  computerSelection) {
         return 'Draw!'
     }
-    /*
-    if (`${playerWin}`) {
-        return playerScore++;            //code for decoration
-    } else if (`${compWin}`) {
-        return compScore++;
-    } */
  };
 
  //function to play the game 5 times
 const game = function() {
     console.log(playRound(playerSelection, computerSelection));
-    
-  
+    console.log(playRound(playerSelection, computerSelection));
+    console.log(playRound(playerSelection, computerSelection));
+    console.log(playRound(playerSelection, computerSelection));
+    console.log(playRound(playerSelection, computerSelection));
 };
 
 game();
