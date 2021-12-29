fedf5b4a021d70c7b429b743c9401df8e9f0e906
enutbrown <erica.nutbrown0424@gmail.com>
2021-12-28 20:14:00 -0800

    declared variables within game function

--- script.js
+++ script.js
@@ -19,32 +19,36 @@ let compScore = 0;
 //plays one round of rock paper scissors, outputs who wins
 let playRound = function(playerSelection, computerSelection) {
     if (playerSelection == 'rock' && computerSelection == 'scissors') {
-        return `${playerWin} Rock beats Scissors!` + ' Player Score: ' + playerScore++;
+        return `${playerWin} Rock beats Scissors!` + ' Player Score: ' + ++playerScore;
     } else if (playerSelection == 'rock' && computerSelection == 'paper') {
-        return `${compWin} Paper beats Rock!` + ' Computer Score: ' + compScore++;
+        return `${compWin} Paper beats Rock!` + ' Computer Score: ' + ++compScore;
     } else if (playerSelection == 'paper' && computerSelection == 'rock') {
-        return `${playerWin} Paper beats Rock!` + ' Player Score: ' + playerScore++;
+        return `${playerWin} Paper beats Rock!` + ' Player Score: ' + ++playerScore;
     } else if (playerSelection == 'paper' && computerSelection == 'scissors') {
-        return `${compWin} Scissors beats Paper!` + ' Computer Score: ' + compScore++;
+        return `${compWin} Scissors beats Paper!` + ' Computer Score: ' + ++compScore;
     } else if (playerSelection == 'scissors' && computerSelection == 'paper') {
-        return `${playerWin} Scissors beats Paper!` + ' Player Score: ' + playerScore++;
+        return `${playerWin} Scissors beats Paper!` + ' Player Score: ' + ++playerScore;
     } else if (playerSelection == 'scissors' && computerSelection == 'rock') {
-        return `${compWin} Rock beats Scissors!` + ' Computer Score: ' + compScore++;
-    } else if (playerSelection == ' ' && computerSelection == 'rock' || 'paper' || 'scissors') {
-        return 'Let\'s Play';
+        return `${compWin} Rock beats Scissors!` + ' Computer Score: ' + ++compScore;
     } else if (playerSelection ===  computerSelection) {
         return 'Draw!'
+    } else {
+        return 'Let\'s Play!'
     }
 
-    let turn;
-    return turn++;
  };
 
  //function to play the game 5 times
 const game = function() {
+    let playerScore = 0;
+    let compScore = 0;
+    const computerSelection = computerPlay();
+    const playerSelection = typedPlayerSelection.toLowerCase();
+
     console.log(playRound(playerSelection, computerSelection));
-  
-};
+    
+
+}
 
 game();
 game();
